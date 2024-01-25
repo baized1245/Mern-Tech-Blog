dc4c8ed05a20b1797e51d090427a250b738a1c16
unknown <you@example.com>
2024-01-25 16:08:14 +0600

    create models

---
+++ backend/models/Post.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+// user Schema
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Post', PostSchema)
